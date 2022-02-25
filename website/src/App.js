429ade745cc2340fae89bb833c004ea2be1b3010
yashikaaggarwal04 <yashika.aggarwal@kgpian.iitkgp.ac.in>
2022-02-25 15:47:22 +0530

    navbar

--- website/src/App.js
+++ website/src/App.js
@@ -1,2 +1,21 @@
+//import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar';
+
+
+function App() {
+  return (
+    <>
+    <div>
+        <h1>Sarojini Naidu/Indira Gandhi Hall of Residence</h1>
+    </div>
+     <Navbar/>
+     
+     
+    </>
+  );
+}
+
+export default App;
+
 
