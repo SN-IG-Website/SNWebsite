e83aeae7e60fcecc9e64bfe4b86aa2d2fa8d888c
25-vidushi <vidushi232@kgpian.iitkgp.ac.in>
2022-03-27 16:14:18 +0530

    Alumni page

---
+++ website/src/pages/alumni.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import AlumniTable from '../components/AlumniTable'
+
+function alumni() {
+    return (
+      <div>
+        <AlumniTable/>
+      </div>
+    )
+  }
+  
+  export default alumni
