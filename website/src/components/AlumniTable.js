f9e128566ffae5fd7ac08acbdcecf3b452d5103d
25-vidushi <vidushi232@kgpian.iitkgp.ac.in>
2022-04-03 23:08:21 +0530

    Alumni Page updated

--- website/src/components/AlumniTable.js
+++ website/src/components/AlumniTable.js
@@ -1,4 +1,3 @@
-
 import React from 'react';
 import "../components/AlumniTable.css"
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -6,19 +5,13 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import { useTable } from "react-table";
+import "../components/AlumniTable.css";
 
 function AlumniTable() {
     return (
         <>
-            <div style={{ display: 'block', padding: 50, backgroundImage: "linear-gradient(150deg,#557B83,#A2D5AB,#E5EFC1", fontSize: "20px" }}>
-                <h4
-                style={{
-                    padding: "30px",
-                    margin: "20px",
-                    textAlign: "center",
-                    fontSize: "50px"
-                }}>Distinguished Alumni</h4>
-                <Row>
+            <div style={{ display: 'block', padding: 50, backgroundImage: "linear-gradient(150deg,#557B83,#A2D5AB,#E5EFC1", fontSize: "20px", fontFamily: "'Gothic A1', sans-serif" }}>
+                <Row style={{}}>
                     <Col style={{
                         textAlign: "center",
                         borderStyle: "solid",
@@ -41,7 +34,7 @@ function AlumniTable() {
                         width: "30%",
                         padding: "15px"
                     }}>
-                        Achievement
+                        Awards
                     </Col>
                 </Row>
                 <Row>
