c6fa0e972c8514ec9445fcca109e0b1140122642
JessicaJohnBritto <jessjohnbritto@gmail.com>
2022-04-04 19:35:15 +0530

    Hall council

--- website/src/App.js
+++ website/src/App.js
@@ -2,6 +2,8 @@
 // import './App.css';
 // import Navbar from './components/Navbar';
 import Teams2 from './components/Teams2';
+import Hall from './components/Hall';
+
 
 import {BrowserRouter as Router} from 'react-router-dom' ;
 
@@ -12,7 +14,7 @@ function App() {
        <center><h1 className="heading">Sarojini Naidu/Indira Gandhi Hall of Residence</h1></center> 
     </div> */}
 
-     <Teams2/>
+     <Hall/>
     </Router>
   );
 }
