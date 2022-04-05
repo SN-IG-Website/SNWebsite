862aba4ab7509d875f3d2b0338eef73ffda52dcf
25-vidushi <vidushi232@kgpian.iitkgp.ac.in>
2022-04-05 16:11:35 +0530

    Update AlumniCards.js

--- website/src/components/AlumniCards.js
+++ website/src/components/AlumniCards.js
@@ -10,15 +10,23 @@ function AlumniCard() {
             }}>
                 <h4 style={
                     {
-                        padding: "30px",
-                        margin: "20px",
                         textAlign: "center",
                         fontSize: "70px",
                     }
                 } > Distinguished Alumni </h4>
             </div>
             <div>
-                <div className="Blogs_reveal Blogs_card1"
+                <div className='Distinguished Alumni Awards'>
+
+                <h4 style={
+                    {
+                        marginTop: "60px",
+                        textAlign: "center",
+                        fontSize: "40px",
+                    }
+                } > Distinguished Alumni Awards </h4>
+                                    <hr/>
+                <div className="Row"
                     style={{
                         padding: "4%",
                         display: "flex",
@@ -35,7 +43,7 @@ function AlumniCard() {
                             alignItems: "center",
                         }}>
                             <img style={{
-                                width: "80%",  padding: "20px", marginLeft: "40px"
+                                height: "300px",  padding: "20px", margin: "0 auto"
                             }} src="https://lh6.googleusercontent.com/7mt2vkpxc1_eMIMs63OnrNuHY-9j2TkZ9WeLRsfYoOHnPL5sV43B6M2JAkywKSaeRk_MZK3K0vI9wjPf4UNP6rkWGARtp3a3WF7Hq3cjBYml-6kADQvZ72fOXwQaqYk_VjQ8jwc" alt='error'/>
                         <h4>PROF SUSMITA SUR KOLAY</h4>
                         <p>
@@ -53,13 +61,14 @@ function AlumniCard() {
                             alignItems: "center",
                         }}>
                             <img style={{
-                                width: "80%",  padding: "20px", marginLeft: "40px"
-                            }} src="https://lh6.googleusercontent.com/7mt2vkpxc1_eMIMs63OnrNuHY-9j2TkZ9WeLRsfYoOHnPL5sV43B6M2JAkywKSaeRk_MZK3K0vI9wjPf4UNP6rkWGARtp3a3WF7Hq3cjBYml-6kADQvZ72fOXwQaqYk_VjQ8jwc" alt='error'/>
-                        <h4>PROF SUSMITA SUR KOLAY</h4>
+                                height: "300px",  padding: "20px", margin: "0 auto"
+                            }} src="https://d3sr7cc30ek8f4.cloudfront.net/images/size:1500x1500/type:cover/prod/617a7995ded0510014acda99/unnamed1.jpg" alt='error'/>
+                        <h4>CHAITALI CHAKRABARTI</h4>
                         <p>
-                        1980/BTech/EC
+                        1984/B Tech/EC
+
                         <br/>
-                        Professor, ACM Unit, Indian Statistical Institute, Kolkata
+                        Professor,Arizona State University (ASU)
                         </p>
                     </div>
                     <div
@@ -71,15 +80,84 @@ function AlumniCard() {
                             alignItems: "center",
                         }}>
                             <img style={{
-                                width: "80%",  padding: "20px", marginLeft: "40px"
-                            }} src="https://lh6.googleusercontent.com/7mt2vkpxc1_eMIMs63OnrNuHY-9j2TkZ9WeLRsfYoOHnPL5sV43B6M2JAkywKSaeRk_MZK3K0vI9wjPf4UNP6rkWGARtp3a3WF7Hq3cjBYml-6kADQvZ72fOXwQaqYk_VjQ8jwc" alt='error'/>
-                        <h4>PROF SUSMITA SUR KOLAY</h4>
+                                height: "300px",  padding: "20px", margin: "0 auto"
+                            }} src="https://lh5.googleusercontent.com/jFl5ABvdfNI53kIUBo7vgJqGCwtB6aW-YvQeRy3_s2Khydx8epkNn1XDJkHep1hUN0WJRqlcE9Va3E8qKJZKJgF-2gvtG8oIJuksBWC1FKrN79vg9NPbHYGs231c" alt='error'/>
+                        <h4>ARUNIMA ACHARYA</h4>
                         <p>
-                        1980/BTech/EC
+                        1973/M Tech/CH
+
                         <br/>
-                        Professor, ACM Unit, Indian Statistical Institute, Kolkata
+                        Advisor to PfP Industries
                         </p>
                     </div>
+                </div>
+
+
+                <div className="Row"
+                    style={{
+                        padding: "4%",
+                        display: "flex",
+                        justifyContent: "space-around",
+                        height: "auto",
+                    }}>
+                    <br /><br />
+                    <div
+                        style={{
+                            padding: "2% 5%",
+                            width: "40%",
+                            margin: "1%",
+                            // color: "#BF9270"
+                            alignItems: "center",
+                        }}>
+                            <img style={{
+                                height: "300px",  padding: "20px", margin: "0 auto"
+                            }} src="https://lh4.googleusercontent.com/Jcn-8qafh4VGUpDHXNgvoMv4E1CFeLBIp1wrTvJK1q28ml1J_NXgBCu3pWa__J-BJwgACzOGFl8CXbHzquNa0CGh8gk11WUfC0PxUIHTpsa4DGxmJYscsYsn2z94" alt='error'/>
+                        <h4>ANURADHA ACHARYA</h4>
+                        <p>
+                        1993/B Sc/PH
+                        <br/>
+                        CEO & Director, Mapmygenome India Limited
+                        </p>
+                    </div>
+                    <div
+                        style={{
+                            padding: "2% 5%",
+                            width: "40%",
+                            margin: "1%",
+                            // color: "#BF9270"
+                            alignItems: "center",
+                        }}>
+                            <img style={{
+                                height: "300px",  padding: "20px", margin: "0 auto"
+                            }} src="https://lh6.googleusercontent.com/fxz70v0_tkluBfegMqUPmVCFybNv7veZCpyWjLfAFCyzb5bL9rx3lgL75mNkMD-1Wpiyd_gyoprAp-QegPwiCevOEWswv_sIi0WWTm7cDWPXcZK5f_I_Pj9qPfqM" alt='error'/>
+                        <h4>SANGHAMITRA BANDYOPADHYAY</h4>
+                        <p>
+                        1994/M Tech/CS
+
+                        <br/>
+                        Director,ISI Kolkata
+                        </p>
+                    </div>
+                    <div
+                        style={{
+                            padding: "2% 5%",
+                            width: "40%",
+                            margin: "1%",
+                            // color: "#BF9270"
+                            alignItems: "center",
+                        }}>
+                            <img style={{
+                                height: "300px",  padding: "20px", margin: "0 auto"
+                            }} src="https://lh5.googleusercontent.com/jFl5ABvdfNI53kIUBo7vgJqGCwtB6aW-YvQeRy3_s2Khydx8epkNn1XDJkHep1hUN0WJRqlcE9Va3E8qKJZKJgF-2gvtG8oIJuksBWC1FKrN79vg9NPbHYGs231c" alt='error'/>
+                        <h4>ARUNIMA ACHARYA</h4>
+                        <p>
+                        1973/M Tech/CH
+
+                        <br/>
+                        Advisor to PfP Industries
+                        </p>
+                    </div>
+                </div>
                 </div>
 
             </div>
