8b9cb1c622c281f5a2597e97ec88c554ff0bd433
25-vidushi <vidushi232@kgpian.iitkgp.ac.in>
2022-04-08 19:39:30 +0530

    ex hall council updated

--- website/src/components/Table.js
+++ website/src/components/Table.js
@@ -2,6 +2,8 @@ import React from 'react';
 import "./Table.css"
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLinkedin } from '@fortawesome/free-brands-svg-icons';
 // import 'https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js';
 // import 'https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js';
 // import 'https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css';
@@ -15,28 +17,72 @@ function Table() {
                     <h4>2021-2022</h4>
                 </div>
                 <Row className='table-row'>
-                <Col><a href='#'>Name</a></Col>
-                    <Col>Batch</Col>
-                    <Col>Awards</Col>
+                    <Col>Pankhuri Saxena</Col>
+                    <Col>Hall President</Col>
+                    <Col><a className='table-linkedin' href=''><FontAwesomeIcon icon={faLinkedin}/></a></Col>
                 </Row>
 
                 <Row className='table-row'>
-                    <Col><a href='#'>Name</a></Col>
-                    <Col>Batch</Col>
-                    <Col>Awards</Col>
+                    <Col>Pooja Bhagwan Arakh</Col>
+                    <Col>Second Senate Member</Col>
+                    <Col><a className='table-linkedin' href=''><FontAwesomeIcon icon={faLinkedin}/></a></Col>
                 </Row>
 
                 <Row className='table-row'>
-                <Col><a href='#'>Name</a></Col>
-                    <Col>Batch</Col>
-                    <Col>Awards</Col>
+                    <Col>Barsa Maju</Col>
+                    <Col>Student Auditor</Col>
+                    <Col><a className='table-linkedin' href=''><FontAwesomeIcon icon={faLinkedin}/></a></Col>
                 </Row>
 
                 <Row className='table-row'>
-                <Col><a href='#'>Name</a></Col>
-                    <Col>Batch</Col>
-                    <Col>Awards</Col>
+                <Col>Radhika Gupta</Col>
+                    <Col>General Secretary Sports and Games</Col>
+                    <Col><a className='table-linkedin' href=''><FontAwesomeIcon icon={faLinkedin}/></a></Col>
                 </Row>
+
+                <Row className='table-row'>
+                <Col>Mansi Sarawata</Col>
+                    <Col>General Secretary Technology</Col>
+                    <Col><a className='table-linkedin' href=''><FontAwesomeIcon icon={faLinkedin}/></a></Col>
+                </Row>
+
+                <Row className='table-row'>
+                <Col>Amrutha Sravya Mamidi</Col>
+                    <Col>General Secretary Social and Cultural</Col>
+                    <Col><a className='table-linkedin' href=''><FontAwesomeIcon icon={faLinkedin}/></a></Col>
+                </Row>
+
+                <Row className='table-row'>
+                <Col>Lorem Ipsum</Col>
+                    <Col>General Secretary Maintainance</Col>
+                    <Col><a className='table-linkedin' href=''><FontAwesomeIcon icon={faLinkedin}/></a></Col>
+                </Row>
+
+                <Row className='table-row'>
+                <Col>Lorem Ipsum</Col>
+                    <Col>General Secretary Student's Welfare</Col>
+                    <Col><a className='table-linkedin' href=''><FontAwesomeIcon icon={faLinkedin}/></a></Col>
+                </Row>
+
+                <Row className='table-row'>
+                <Col>Lorem Ipsum</Col>
+                    <Col>General Secretary Alumni Affairs</Col>
+                    <Col><a className='table-linkedin' href=''><FontAwesomeIcon icon={faLinkedin}/></a></Col>
+                </Row>
+
+                <Row className='table-row'>
+                <Col>Lorem Ipsum</Col>
+                    <Col>General Secretary Mess</Col>
+                    <Col><a className='table-linkedin' href=''><FontAwesomeIcon icon={faLinkedin}/></a></Col>
+                </Row>
+
+                <Row className='table-row'>
+                <Col>Lorem Ipsum</Col>
+                    <Col>General Secretary Library</Col>
+                    <Col><a className='table-linkedin' href=''><FontAwesomeIcon icon={faLinkedin}/></a></Col>
+                </Row>
+
+
 
             </div>
         </>
