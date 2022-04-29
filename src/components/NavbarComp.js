2797f686398fb11a18ba67ff9394079b2a9743f8
25-vidushi <vidushi232@kgpian.iitkgp.ac.in>
2022-04-30 00:03:59 +0530

    navbar color updated

--- src/components/NavbarComp.js
+++ src/components/NavbarComp.js
@@ -27,30 +27,43 @@ function NavbarComp() {
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
-              <Nav.Link href="/">Home</Nav.Link>
-              <NavDropdown title="" id="basic-nav-dropdown">
+              <Nav.Link style={{
+                color: "#FBE5E5"
+              }} href="/">Home</Nav.Link>
+              {/* <NavDropdown title="" id="basic-nav-dropdown">
                 <NavDropdown.Item href="#aboutsnig">About</NavDropdown.Item>
                 <NavDropdown.Item href="#infrasnig">
                   Infrastructure
                 </NavDropdown.Item>
-                {/* <NavDropdown.Item href="#action/3.3">
+                <NavDropdown.Item href="#action/3.3">
                   Something
                 </NavDropdown.Item>
                 <NavDropdown.Divider />
                 <NavDropdown.Item href="#action/3.4">
                   Separated link
-                </NavDropdown.Item> */}
-              </NavDropdown>
-              <Nav.Link href="hall">Office bearers</Nav.Link>
-              <Nav.Link href="alumni">Alumni</Nav.Link>
-              <NavDropdown title="" id="basic-nav-dropdown">
+                </NavDropdown.Item>
+              </NavDropdown> */}
+              <Nav.Link style={{
+                color: "#FBE5E5"
+              }} href="hall">Office bearers</Nav.Link>
+              <Nav.Link 
+              style={{
+                color: "#FBE5E5"
+              }} className="navLink" href="alumni">Alumni</Nav.Link>
+              {/* <NavDropdown title="" id="basic-nav-dropdown">
                 <NavDropdown.Item href="#Distinguished Alumni Awards">Distinguished Alumni Awards</NavDropdown.Item>
                 <NavDropdown.Item href="#Distinguished Service Awards">Distinguished Service Awards</NavDropdown.Item>
                 <NavDropdown.Item href="#Young Alumni Achiever Awards">Young Alumni Achiever Awards</NavDropdown.Item>
-              </NavDropdown>
-              <Nav.Link href="gallery">Gallery</Nav.Link>
-              <Nav.Link href="achievements">Achievements</Nav.Link>
-              <Nav.Link href="teams2">Our Team</Nav.Link>
+              </NavDropdown> */}
+              <Nav.Link style={{
+                color: "#FBE5E5"
+              }} href="gallery">Gallery</Nav.Link>
+              <Nav.Link style={{
+                color: "#FBE5E5"
+              }} href="achievements">Achievements</Nav.Link>
+              <Nav.Link style={{
+                color: "#FBE5E5"
+              }} href="teams2">Our Team</Nav.Link>
 
 
             </Nav>
