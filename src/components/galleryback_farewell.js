caff03e1769aa54bd9ea94667e579415b33852eb
Siddhi-Agrawal21 <82286305+Siddhi-Agrawal21@users.noreply.github.com>
2022-05-03 01:20:54 +0530

    changes made

--- src/components/galleryback_farewell.js
+++ src/components/galleryback_farewell.js
@@ -2,16 +2,63 @@ import React from "react";
 import "./galleryback.css";
 import NavbarComp from './NavbarComp';
 import Footer from '../components/footer';
-import alumni1 from "./images/alumni1.jpg" ;
-import alumni2 from "./images/alumni2.jpg" ;
-import alumni3 from "./images/alumni3.jpg" ;
-import alumni4 from "./images/alumni4.jpg" ;
-import alumni5 from "./images/alumni5.jpg" ;
-import alumni6 from "./images/alumni6.jpg" ;
-import alumni7 from "./images/alumni7.jpg" ;
-import alumni8 from "./images/alumni8.jpg" ;
-import alumni9 from "./images/alumni9.jpg" ;
-import alumni10 from "./images/alumni10.jpg" ;
+import fare1 from "./images/IMG_1.jpg" ;
+import fare2 from "./images/IMG_2.jpg" ;
+import fare3 from "./images/IMG_3.jpg" ;
+import fare4 from "./images/IMG_4.jpg" ;
+import fare5 from "./images/IMG_5.jpg" ;
+import fare6 from "./images/IMG_6.jpg" ;
+import fare7 from "./images/IMG_7.jpg" ;
+import fare8 from "./images/IMG_8.jpg" ;
+import fare9 from "./images/IMG_9.jpg" ;
+import fare10 from "./images/IMG_10.jpg" ;
+import fare11 from "./images/IMG_11.jpg" ;
+import fare12 from "./images/IMG_12.jpg" ;
+import fare13 from "./images/IMG_13.jpg" ;
+import fare14 from "./images/IMG_14.jpg" ;
+import fare15 from "./images/IMG_15.jpg" ;
+import fare16 from "./images/IMG_16.jpg" ;
+import fare17 from "./images/IMG_17.jpg" ;
+import fare18 from "./images/IMG_18.jpg" ;
+import fare19 from "./images/IMG_19.jpg" ;
+import fare20 from "./images/IMG_20.jpg" ;
+import fare21 from "./images/IMG_21.jpg" ;
+import fare22 from "./images/IMG_22.jpg" ;
+import fare23 from "./images/IMG_23.jpg" ;
+import fare24 from "./images/IMG_24.jpg" ;
+import fare25 from "./images/IMG_25.jpg" ;
+import fare26 from "./images/IMG_26.jpg" ;
+import fare27 from "./images/IMG_27.jpg" ;
+import fare28 from "./images/IMG_28.jpg" ;
+import fare29 from "./images/IMG_29.jpg" ;
+import fare30 from "./images/IMG_30.jpg" ;
+import fare31 from "./images/IMG_31.jpg" ;
+import fare32 from "./images/IMG_32.jpg" ;
+import fare33 from "./images/IMG_33.jpg" ;
+import fare34 from "./images/IMG_34.jpg" ;
+import fare35 from "./images/IMG_35.jpg" ;
+import fare36 from "./images/IMG_36.jpg" ;
+import fare37 from "./images/IMG_37.jpg" ;
+import fare38 from "./images/IMG_38.jpg" ;
+import fare39 from "./images/IMG_39.jpg" ;
+import fare40 from "./images/IMG_40.jpg" ;
+import fare41 from "./images/IMG_41.jpg" ;
+import fare42 from "./images/IMG_42.jpg" ;
+import fare43 from "./images/IMG_43.jpg" ;
+import fare44 from "./images/IMG_44.jpg" ;
+import fare45 from "./images/IMG_45.jpg" ;
+import fare46 from "./images/IMG_46.jpg" ;
+import fare47 from "./images/IMG_47.jpg" ;
+import fare48 from "./images/IMG_48.jpg" ;
+import fare49 from "./images/IMG_49.jpg" ;
+import fare50 from "./images/IMG_50.jpg" ;
+import fare51 from "./images/IMG_51.jpg" ;
+import fare52 from "./images/IMG_52.jpg" ;
+import fare53 from "./images/IMG_53.jpg" ;
+import fare54 from "./images/IMG_54.jpg" ;
+import fare55 from "./images/IMG_55.jpg" ;
+import fare56 from "./images/IMG_56.jpg" ;
+
 
 function galleryback() {
   return (
@@ -19,25 +66,71 @@ function galleryback() {
     <center><div className="heading"><h1 className="backimagecontent">Farewell</h1></div></center>
       <div className="row">
         <div className="column">
-          <img src={alumni1} />
-          <img src={alumni5} />
-          <img src={alumni9} />
+          <img src={fare1} />
+          <img src={fare2} />
+          <img src={fare3} />
+          <img src={fare4} />
+          <img src={fare5} />
+          <img src={fare6} />
+          <img src={fare7} />
+          <img src={fare8} />
+          <img src={fare9} />
+          <img src={fare10} />
+          <img src={fare11} />
+          <img src={fare12} />
+          <img src={fare13} />
+          <img src={fare14} />
+        
      
         </div>
         <div className="column">
-          <img src={alumni2} />
-          <img src={alumni6} />
-         
+        
+          <img src={fare15} />
+          <img src={fare16} />
+          <img src={fare17} />
+          <img src={fare18} />
+          <img src={fare19} />
+          <img src={fare20} />
+          <img src={fare21} />
+          <img src={fare22} />
+          <img src={fare23} />
+          <img src={fare24} />
+          <img src={fare25} />
+          <img src={fare26} />
+          <img src={fare27} />
+          <img src={fare28} />
         </div>
         <div className="column">
-          <img src={alumni3} />
-          <img src={alumni7} />
-
+          <img src={fare29} />
+          <img src={fare30} />
+          <img src={fare31} />
+          <img src={fare32} />
+          <img src={fare33} />
+          <img src={fare34} />
+          <img src={fare35} />
+          <img src={fare36} />
+          <img src={fare37} />
+          <img src={fare38} />
+          <img src={fare39} />
+          <img src={fare40} />
+          <img src={fare41} />
+          <img src={fare42} />
         </div>
         <div className="column">
-          <img src={alumni4} />
-          <img src={alumni8} />
-          <img src={alumni10} />
+          <img src={fare43} />
+          <img src={fare44} />
+          <img src={fare45} />
+          <img src={fare46} />
+          <img src={fare47} />
+          <img src={fare48} />
+          <img src={fare49} />
+          <img src={fare50} />
+          <img src={fare51} />
+          <img src={fare52} />
+          <img src={fare53} />
+          <img src={fare54} />
+          <img src={fare55} />
+          <img src={fare56} />
           
         </div>
       </div>
