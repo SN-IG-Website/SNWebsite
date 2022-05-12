3cf1e2374cc0fa2f4bb308faac3cb830ca9d54f1
yashikaaggarwal04 <yashika.aggarwal@kgpian.iitkgp.ac.in>
2022-05-12 22:34:14 +0530

    changes

--- src/components/galleryback_granddinner.js
+++ src/components/galleryback_granddinner.js
@@ -13,7 +13,34 @@ import Footer from '../components/footer';
 function galleryback() {
   return (
     <><NavbarComp/>
-     <center><div className="heading"><h1 className="backimagecontent" >Grand Dinner</h1></div></center>
+     {/* <center><div className="heading"><h1 className="backimagecontent" >Grand Dinner</h1></div></center> */}
+     <div className="text">
+          <div
+            className="Main-header"
+            style={{
+              paddingTop: "70px",
+              paddingBottom: "0px",
+              // backgroundColor: "#E3B7A0",
+              // backgroundImage: "linear-gradient(150deg,#E3B7A0,#BF9270",
+            }}
+         / ></div>
+     <div className="site-heading text-center" style={{marginBottom:'5px'}}>
+              <h5
+                className="heading"
+                style={{
+                  textAlign: "center",
+                  fontSize: "50px",
+                  marginTop: "30px",
+                  padding: "0 px",
+                  fontWeight: "700",
+                }}
+              >
+                {" "}
+                Grand Dinner{" "}
+              </h5>
+              
+              <h4 style={{paddingBottom:'0px'}}> </h4>
+            </div>
       <div className="row">
         <div className="column">
           <img alt="" src={granddinner1} />
